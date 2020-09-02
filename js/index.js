60608ad9c50501ece6723a7ce19513615bc0bd47
ssshoshi <jlschultz88@gmail.com>
2020-09-01 21:59:46 -0400

    contact

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
-
-
+let scrollTo = () => {
+document.getElementById('contact').scrollIntoView({
+    behavior: 'smooth', // smooth scroll
+    block: 'start' // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
+  })
+}
