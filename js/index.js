35f85191f47e143614043365a71ceec458b551d2
ssshoshi <jlschultz88@gmail.com>
2020-09-07 18:07:33 -0400

    add homescanner project

--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
-let scrollTo = () => {
-document.getElementById('contact').scrollIntoView({
-    behavior: 'smooth', // smooth scroll
-    block: 'start' // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
-  })
-}
-
 document.addEventListener('DOMContentLoaded', () => {
 
   // Get all "navbar-burger" elements
